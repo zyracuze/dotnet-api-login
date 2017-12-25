23f7553bfe00c9c53a6f6e49b97b388b9f1993d1
kurozakizz <squallcope@gmail.com>
2017-12-25 16:56:08 +0700

    [ADD] Create LoginController and Unit test for Acceptance test login success

---
+++ src/api/Controllers/LoginController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+using api.Models;
+
+
+namespace api.Controllers
+{
+    [Route("api/login")]
+    public class LoginController : Controller
+    {
+
+        [HttpPost()]
+        public ResponseMessage Post([FromBody]User user)
+        {
+
+           User expected_user = new User(){
+               Id = 1,
+               Username = "ploy",
+               Displayname = "พลอย"
+           };
+
+                return new ResponseMessage(){
+                    Status = "OK",
+                    Results = expected_user
+                }; 
+
+
+
+        }
+    }
+}
---
+++ tests/api.UnitTest/LoginControllerTest.cs
@@ -0,0 +1,26 @@
+using System;
+using Microsoft.AspNetCore.Mvc;
+using Xunit;
+using api.Controllers;
+using api.Models;
+
+namespace api.UnitTest
+{
+    public class LoginControllerTest
+    {
+        [Fact]
+        public void PostLogin_ResturnJSON_WhenLoginIsSuccessful(){
+
+            var loginController = new LoginController();
+            var result = loginController.Post( new User(){
+                Username = "ploy",
+                Password = "Sck1234"
+            });
+
+            var reponse = Assert.IsType<ResponseMessage>(result);
+            var model = Assert.IsAssignableFrom<User>(reponse.Results);
+            Assert.Equal("ploy", model.Username);
+            Assert.Equal("พลอย", model.Displayname);
+        }
+    }
+}
