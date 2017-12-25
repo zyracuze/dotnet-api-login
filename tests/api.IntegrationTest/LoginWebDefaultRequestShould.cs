6dd80f4ef2c9b4825d6e3df289065ce196c14111
kurozakizz <squallcope@gmail.com>
2017-12-25 17:19:47 +0700

    [ADD] สร้าง IntegationTest login success

---
+++ tests/api.IntegrationTest/LoginWebDefaultRequestShould.cs
@@ -0,0 +1,44 @@
+using System;
+using Xunit;
+
+using System.Net;
+using System.Net.Http;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+
+using System.Text;
+using Newtonsoft.Json;
+using api.Models;
+
+namespace api.IntegrationTest
+{
+  public class LoginWebDefaultRequestShould
+  {
+    private readonly TestServer server;
+    private readonly HttpClient client;
+
+    public LoginWebDefaultRequestShould()
+    {
+      // Arrange
+      server = new TestServer(new WebHostBuilder().UseStartup<Startup>());
+      client = server.CreateClient();
+    }
+
+    [Fact]
+    public async void ReturnUserInfoGivenLoginInfo()
+    {
+      var request = "/api/login";
+      String jsonData = "{ \"username\": \"ploy\", \"password\": \"Sck1234\"}";
+      HttpContent payload = new StringContent(jsonData, Encoding.UTF8, "application/json");
+      var response = await client.PostAsync(request, payload);
+      response.EnsureSuccessStatusCode();
+
+
+      ResponseMessage results = JsonConvert.DeserializeObject<ResponseMessage>(await response.Content.ReadAsStringAsync());
+
+      Assert.Equal("OK", results.Status);
+      Assert.Equal("ploy", results.Results.Username);
+      Assert.Equal("พลอย", results.Results.Displayname);
+    }
+  }
+} 
