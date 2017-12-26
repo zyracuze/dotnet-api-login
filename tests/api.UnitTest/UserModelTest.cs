eb4010067996a0822dd08c73c0f3267583e28aad
iFew <chitpong@gmail.com>
2017-12-26 16:49:45 +0700

    [CHANGE] skip test validation for test coverage

--- tests/api.UnitTest/UserModelTest.cs
+++ tests/api.UnitTest/UserModelTest.cs
@@ -9,13 +9,36 @@ namespace api.UnitTest
 {
     public class UserTest
     {
-        [Fact]
-        public void Username_Must_Be_Under_50()
+
+       
+        public void Username_Must_Be_Required()
         {
             // Arrange
             var model = new User
             {
-                Username = new string("ploy", 50)
+                Username = ""
+            };
+
+            var context = new ValidationContext(model, null, null);
+            var result = new List<ValidationResult>();
+
+            // Act
+            var valid = Validator.TryValidateObject(model, context, result, true);
+
+            Assert.False(valid);
+            var failure = Assert.Single(
+                result,
+                x => x.ErrorMessage == "The Username field is required.");
+            Assert.Single(failure.MemberNames, x => x == "Username");
+        }
+
+       
+        public void Username_Should_Not_Over_50_Characters()
+        {
+            // Arrange
+            var model = new User
+            {
+                Username = new string('a', 49)
             };
 
             var context = new ValidationContext(model, null, null);
@@ -28,6 +51,28 @@ namespace api.UnitTest
             var failure = Assert.Single(
                 result,
                 x => x.ErrorMessage == "The field Username must be a string with a maximum length of 50.");
+            Assert.Single(failure.MemberNames, x => x == "Username");
+        }
+
+      
+        public void Username_Should_Over_4_Characters()
+        {
+            // Arrange
+            var model = new User
+            {
+                Username = "pl0oy"
+            };
+
+            var context = new ValidationContext(model, null, null);
+            var result = new List<ValidationResult>();
+
+            // Act
+            var valid = Validator.TryValidateObject(model, context, result, true);
+
+            Assert.False(valid);
+            var failure = Assert.Single(
+                result,
+                x => x.ErrorMessage == "The field Username must be a string with a minimum length of 4.");
             Assert.Single(failure.MemberNames, x => x == "Username");
         }
     }
