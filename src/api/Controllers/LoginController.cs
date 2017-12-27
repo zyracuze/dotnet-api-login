3d40f662a9b1a94ae635f76f7ad5c93e278fd862
kurozakizz <squallcope@gmail.com>
2017-12-27 17:08:41 +0700

    [CHANGE] แก้ไข validate empty string ให้เรียกจาก IsUsernameOrPasswordEmpty

--- src/api/Controllers/LoginController.cs
+++ src/api/Controllers/LoginController.cs
@@ -10,57 +10,52 @@ using api.Exceptions;
 
 namespace api.Controllers
 {
-  [Route("api/login")]
-  public class LoginController : Controller
-  {
-    private readonly AuthenticationService authenticationService;
-
-    public LoginController(AuthenticationService service)
+    [Route("api/login")]
+    public class LoginController : Controller
     {
-      authenticationService = service;
-    }
+        private readonly AuthenticationService authenticationService;
 
-    [HttpPost()]
-    public ResponseMessage Post([FromBody]User user)
-    {
-
-      if (UsernameOrPasswordIsEmpty(user.Username, user.Password))
-      {
-        return GenerateResponseMessage("ERROR", "Username and Password are required.");
-      }
-
-      try
-      {
-        User resultUser = authenticationService.Login(user.Username, user.Password);
-        return GenerateResponseMessage("OK", resultUser);
-      }
-      catch (UserNotFoundException ex)
-      {
-        return GenerateResponseMessage("ERROR", ex.Message);
-      }
-
-    }
-
-    private bool UsernameOrPasswordIsEmpty(string username, string password)
-    {
-      return string.IsNullOrEmpty(username) || string.IsNullOrEmpty(password);
-    }
-
-    private ResponseMessage GenerateResponseMessage(string status, string message)
-    {
-      return new ResponseMessage()
+        public LoginController(AuthenticationService service)
         {
-          Status = status ,
-          Message = message
-        };
-    }
-    private ResponseMessage GenerateResponseMessage(string status, User user)
-    {
-      return new ResponseMessage()
+            authenticationService = service;
+        }
+
+        [HttpPost()]
+        public ResponseMessage Post([FromBody]User user)
         {
-          Status = status ,
-          Results = user 
-        };
+            if (authenticationService.IsUsernameOrPasswordEmpty(user.Username, user.Password))
+            {
+                return GetErrorMessage("ERROR", "Username and Password are required.");
+            }
+
+            try
+            {
+                User resultUser = authenticationService.Login(user.Username, user.Password);
+                return GetOkMessage("OK", resultUser);
+            }
+            catch (UserNotFoundException ex)
+            {
+                return GetErrorMessage("ERROR", ex.Message);
+            }
+
+        }
+
+        private ResponseMessage GetErrorMessage(string status, string message)
+        {
+            return GenerateResponseMessage(status, message, null);
+        }
+        private ResponseMessage GetOkMessage(string status, User user)
+        {
+            return GenerateResponseMessage(status, null, user);
+        }
+        private ResponseMessage GenerateResponseMessage(string status, string message, User user)
+        {
+            return new ResponseMessage()
+            {
+                Status = status,
+                Message = message,
+                Results = user
+            };
+        }
     }
-  }
 }
