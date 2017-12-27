96e42b0d5a23584eb23541a31480f34c26fbd29b
kurozakizz <squallcope@gmail.com>
2017-12-27 17:06:05 +0700

    [ADD] สร้าง unit test ของ method IsUsernameOrPasswordEmpty

--- tests/api.UnitTest/Services/AuthenticationServiceTest.cs
+++ tests/api.UnitTest/Services/AuthenticationServiceTest.cs
@@ -8,82 +8,97 @@ using api.UnitTest.Contexts;
 namespace api.UnitTest.Services
 {
 
-  public class AuthenticationServiceTest
-  {
-    [Fact]
-    public void Login_CorrectUser_ReturnsExpectedUser()
+    public class AuthenticationServiceTest
     {
-      var username = "ploy";
-      var hashPassword = "2ebaee7d310c99cb38e069a7014d64bd4b043caa1646489bdc4b5f07f3479585";
+        [Fact]
+        public void Login_CorrectUser_ReturnsExpectedUser()
+        {
+            var username = "ploy";
+            var hashPassword = "2ebaee7d310c99cb38e069a7014d64bd4b043caa1646489bdc4b5f07f3479585";
 
-      var expectedUser = new User(){
-          Id = 1,
-          Username = "ploy",
-          Displayname = "พลอย"
-      };
+            var expectedUser = new User()
+            {
+                Id = 1,
+                Username = "ploy",
+                Displayname = "พลอย"
+            };
 
-      StubSuccessUserContext stubSuccessUserContext = new StubSuccessUserContext();
-      AuthenticationService authenticationService = new AuthenticationService(stubSuccessUserContext);
+            StubSuccessUserContext stubSuccessUserContext = new StubSuccessUserContext();
+            AuthenticationService authenticationService = new AuthenticationService(stubSuccessUserContext);
 
-      User actualUser = authenticationService.Login(username, hashPassword);
+            User actualUser = authenticationService.Login(username, hashPassword);
 
-      // Assert
-      Assert.IsType<User>(actualUser);
-      Assert.Equal(expectedUser.Id, actualUser.Id);
-      Assert.Equal(expectedUser.Username, actualUser.Username);
-      Assert.Equal(expectedUser.Displayname, actualUser.Displayname);
+            // Assert
+            Assert.IsType<User>(actualUser);
+            Assert.Equal(expectedUser.Id, actualUser.Id);
+            Assert.Equal(expectedUser.Username, actualUser.Username);
+            Assert.Equal(expectedUser.Displayname, actualUser.Displayname);
 
+        }
+
+        [Fact]
+        public void Login_NotExistUser_ThrowExceptionUserNotFound()
+        {
+            var username = "ploy";
+            var hashPassword = "Sck1234";
+
+            var expectedUser = new User()
+            {
+                Id = 1,
+                Username = "ploy",
+                Displayname = "พลอย"
+            };
+
+            StubThrowExceptionUserContext stubUserContextThrowException = new StubThrowExceptionUserContext();
+            AuthenticationService authenticationService = new AuthenticationService(stubUserContextThrowException);
+
+            try
+            {
+
+                User actualUser = authenticationService.Login(username, hashPassword);
+                Assert.True(false, "UserNotFoundException was not thrown");
+
+            }
+            catch (UserNotFoundException)
+            {
+                // Assert
+                Assert.True(true);
+            }
+
+        }
+
+        [Theory]
+        [InlineData("", "Sck1234")]
+        [InlineData("ploy", "")]
+        public void Login_NotExistUser_ThrowArgumentException(string username, string hashPassword)
+        {
+            StubSuccessUserContext stubSuccessUserContext = new StubSuccessUserContext();
+            AuthenticationService authenticationService = new AuthenticationService(stubSuccessUserContext);
+
+            try
+            {
+                User actualUser = authenticationService.Login(username, hashPassword);
+                Assert.True(false, "ArgumentException was not thrown");
+            }
+            catch (ArgumentException)
+            {
+                // Assert
+                Assert.True(true);
+            }
+        }
+
+        [Theory]
+        [InlineData("", "Sck1234")]
+        [InlineData("ploy", "")]
+        [InlineData("", "")]
+        public void IsUsernameOrPasswordEmpty_EmpytyUsernameOrPassword_ReturnsFalse(string username, string password)
+        {
+            StubSuccessUserContext stubSuccessUserContext = new StubSuccessUserContext();
+            AuthenticationService authenticationService = new AuthenticationService(stubSuccessUserContext);
+
+            bool isValid = authenticationService.IsUsernameOrPasswordEmpty(username, password);
+
+            Assert.True(isValid);
+        }
     }
-
-    [Fact]
-    public void Login_NotExistUser_ThrowExceptionUserNotFound()
-    {
-      var username = "ploy";
-      var hashPassword = "Sck1234";
-
-      var expectedUser = new User(){
-          Id = 1,
-          Username = "ploy",
-          Displayname = "พลอย"
-      };
-
-      StubThrowExceptionUserContext stubUserContextThrowException = new StubThrowExceptionUserContext();
-      AuthenticationService authenticationService = new AuthenticationService(stubUserContextThrowException);
-
-      try
-      {
-
-        User actualUser = authenticationService.Login(username, hashPassword);
-        Assert.True(false, "UserNotFoundException was not thrown");
-
-      }
-      catch (UserNotFoundException)
-      {
-        // Assert
-        Assert.True(true);
-      }
-
-    }
-
-    [Theory]
-    [InlineData("","Sck1234")]
-    [InlineData("ploy","")]
-    public void Login_NotExistUser_ThrowArgumentException(string username, string hashPassword)
-    {
-      StubSuccessUserContext stubSuccessUserContext = new StubSuccessUserContext();
-      AuthenticationService authenticationService = new AuthenticationService(stubSuccessUserContext);
-
-      try
-      {
-        User actualUser = authenticationService.Login(username, hashPassword);
-        Assert.True(false, "ArgumentException was not thrown");
-      }
-      catch (ArgumentException)
-      {
-        // Assert
-        Assert.True(true);
-      }
-    }
-    
-  }
 }
